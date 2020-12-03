3d650e9280e7bf398c4779ba11d5b613dbcffd79
rizkyakhid <rizkyakhid@gmail.com>
2020-12-04 00:40:48 +0700

    add entertainMe route

--- orchestrators/expressOrchestrator/controllers/entertainMeController.js
+++ orchestrators/expressOrchestrator/controllers/entertainMeController.js
@@ -1,8 +1,22 @@
 const axios = require('axios')
+const movieUrl = process.env.movieUrl || 'http://localhost:3004/'
+const seriesUrl = process.env.seriesUrl || 'http://localhost:3002/'
 
 class EntertainMeController {
-  static getAll(req, res) {
-
+  static async getAll(req, res) {
+    try {
+      const movies = await axios({
+        method: 'get',
+        url: movieUrl
+      })
+      const series = await axios({
+        method: 'get',
+        url: seriesUrl
+      })
+      res.status(200).json({ movies: movies.data, series: series.data })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
--- orchestrators/expressOrchestrator/routes/index.js
+++ orchestrators/expressOrchestrator/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const EntertainMeController = require('../controllers/entertainMeController')
 const movieRouter = require('./movies')
 const seriesRouter = require('./series')
 
+router.get('/', EntertainMeController.getAll)
 router.use('/movies', movieRouter)
 router.use('/series', seriesRouter)
 
